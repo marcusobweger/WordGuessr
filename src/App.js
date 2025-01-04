54ac09dff2752248f263f08e77e899e1aade5e19
MarcusObw <marcusobweger@gmail.com>
2025-01-04 21:31:45 +0100

    commit 04/01 random words + translation API added, work in progress

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import Play from "./Play";
+import About from "./About";
+import RandomWords from "./RandomWords";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/play" element={<Play />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
