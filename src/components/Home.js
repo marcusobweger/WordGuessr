09b858a93cde6c626ad07af007e8ff9a4f958e3b
MarcusObw <marcusobweger@gmail.com>
2025-01-06 01:45:13 +0100

    commit 06/01 styling to home component added via bootstrap and custom css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import japan from "../icons/japan.png";
+import korea from "../icons/south-korea.png";
+import germany from "../icons/germany.png";
+import italy from "../icons/italy.png";
+import france from "../icons/france.png";
+import spain from "../icons/spain.png";
+import "../styling/Home.css";
+import logo from "../icons/wordguessr_logo1.png";
 
 function Home() {
+  const [gamemode, setGamemode] = useState(0);
   const [sourceLang, setSourceLang] = useState("en");
   const [targetLang, setTargetLang] = useState("ja");
   const [wordCount, setWordCount] = useState(3);
@@ -73,27 +82,102 @@ function Home() {
   };
 
   return (
-    <div>
-      <div className="gamemode">
-        <button>Solo</button>
-        <button>Online</button>
+    <div className="container">
+      <div className="header-container">
+        <img className="logo" src={logo} alt="logo"></img>
+        <h1 className="title">WordGuessr</h1>
       </div>
-      <div className="language">
-        <button onClick={() => setTargetLang("ja")}>ja</button>
-        <button onClick={() => setTargetLang("ko")}>ko</button>
-        <button onClick={() => setTargetLang("de")}>de</button>
-        <button onClick={() => setTargetLang("it")}>it</button>
+      <div className="container">
+        <div className="row mb-3 gap-3">
+          <button
+            className={`col gamemode ${gamemode === 0 ? "clicked" : ""}`}
+            onClick={() => setGamemode(0)}
+            disabled={isLoading}>
+            Solo
+          </button>
+          <button
+            className={`col gamemode ${gamemode === 1 ? "clicked" : ""}`}
+            onClick={() => setGamemode(1)}
+            disabled={true}>
+            Online
+          </button>
+        </div>
+        <div className="row mb-3 gap-3">
+          <button
+            className={`col language ${targetLang === "ja" ? "clicked" : ""}`}
+            disabled={isLoading}
+            onClick={() => setTargetLang("ja")}>
+            <img className="icons" src={japan} alt="japanese"></img>
+          </button>
+          <button
+            className={`col language ${targetLang === "ko" ? "clicked" : ""}`}
+            disabled={isLoading}
+            onClick={() => setTargetLang("ko")}>
+            <img className="icons" src={korea} alt="korean"></img>
+          </button>
+          <button
+            className={`col language ${targetLang === "de" ? "clicked" : ""}`}
+            disabled={isLoading}
+            onClick={() => setTargetLang("de")}>
+            <img className="icons" src={germany} alt="german"></img>
+          </button>
+          <button
+            className={`col language ${targetLang === "it" ? "clicked" : ""}`}
+            disabled={isLoading}
+            onClick={() => setTargetLang("it")}>
+            <img className="icons" src={italy} alt="italian"></img>
+          </button>
+          <button
+            className={`col language ${targetLang === "fr" ? "clicked" : ""}`}
+            disabled={isLoading}
+            onClick={() => setTargetLang("fr")}>
+            <img className="icons" src={france} alt="french"></img>
+          </button>
+          <button
+            className={`col language ${targetLang === "es" ? "clicked" : ""}`}
+            disabled={isLoading}
+            onClick={() => setTargetLang("es")}>
+            <img className="icons" src={spain} alt="spanish"></img>
+          </button>
+        </div>
+        <div className="row mb-3 gap-3">
+          <button
+            className={`col wordCount ${wordCount === 3 ? "clicked" : ""}`}
+            disabled={isLoading}
+            onClick={() => setWordCount(3)}>
+            3
+          </button>
+          <button
+            className={`col wordCount ${wordCount === 5 ? "clicked" : ""}`}
+            disabled={isLoading}
+            onClick={() => setWordCount(5)}>
+            5
+          </button>
+          <button
+            className={`col wordCount ${wordCount === 10 ? "clicked" : ""}`}
+            disabled={isLoading}
+            onClick={() => setWordCount(10)}>
+            10
+          </button>
+          <button
+            className={`col wordCount ${wordCount === 15 ? "clicked" : ""}`}
+            disabled={isLoading}
+            onClick={() => setWordCount(15)}>
+            15
+          </button>
+        </div>
+        <div className="play row mb-3 gap-3 justify-content-md-center">
+          <button className="col" onClick={handlePlay}>
+            {isLoading ? (
+              <div className="spinner-border text-light" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </div>
+            ) : (
+              "Play"
+            )}
+          </button>
+        </div>
       </div>
-      <div className="wordcount">
-        <button onClick={() => setWordCount(3)}>3</button>
-        <button onClick={() => setWordCount(5)}>5</button>
-        <button onClick={() => setWordCount(10)}>10</button>
-        <button onClick={() => setWordCount(15)}>15</button>
-      </div>
-      <div className="play">
-        <button onClick={handlePlay}>PLAY</button>
-      </div>
-      {isLoading && <div className="loading-icon">Loading...</div>}
     </div>
   );
 }
