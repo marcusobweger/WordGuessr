7a06e97d55165eedc20eb1a9da41a87a1c79d69b
marcusobweger <marcusobweger@gmail.com>
2025-02-19 23:00:46 +0100

    started with code documentation

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// configuration for firebase integration
 const firebaseConfig = {
   apiKey: "AIzaSyAxlOni3H55zUX5LAYjvjiZi6WsSBY8Nec",
   authDomain: "wordguessr-765ac.firebaseapp.com",
@@ -11,6 +12,8 @@ const firebaseConfig = {
   appId: "1:399645604891:web:9b652c37330166d9208cbe",
 };
 
+// initialize firebase
 const app = initializeApp(firebaseConfig);
+// export important firebase services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
